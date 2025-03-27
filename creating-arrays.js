32cefcc5ee1f69ad46dfe5280b3946b5c5b2cbfb
prestobailey <prestonscottbailey@gmail.com>
2025-03-27 05:16:26 -0500

    Finished Commit

---
+++ creating-arrays.js
@@ -0,0 +1,27 @@
+// 5 Item Grocery List
+let groceryList = [
+    'bread',
+    'milk',
+    'cheese',
+    'beef',
+    'lettuce'
+];
+
+// 3 Grocery Prices
+let groceryPrices = [1.99, 4.50, 3.29];
+
+// Log entire grocery item array
+console.log(groceryList);
+
+//Log entire grocery prices array
+console.log(groceryPrices);
+
+// Log three individual grocery items
+console.log(groceryList[0]);
+console.log(groceryList[2]);
+console.log(groceryList[4]);
+
+// Log two grocery prices
+console.log(groceryPrices[0]);
+console.log(groceryPrices[2]);
+
